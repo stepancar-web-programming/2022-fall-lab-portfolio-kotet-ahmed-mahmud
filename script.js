67417ad7f0edee132760e0e11d7b90f69491a22b
Ahmed Mahmoud <92227588+sultan0-0-0@users.noreply.github.com>
2022-12-02 22:48:08 +0300

    jquery to js

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-$(document).ready(() => {
+document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line func-names
-  $('#menu').click(function () {
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on('scroll load', () => {
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if ($(window).scrollTop() > 0) {
-      $('.top').show();
-    } else {
-      $('.top').hide();
-    }
-  });
-
-  // eslint-disable-next-line func-names
-  $('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
-
-    $('html, body').animate(
-      {
-
-        scrollTop: $($(this).attr('href')).offset().top,
-
-      },
-      500,
-      'linear',
-    );
+  document.querySelector('#menu').onclick = function () {
+    this.classList.toggle('fa-times');
+    document.querySelector('header').classList.toggle('toggle');
+  };
+  window.addEventListener('scroll', () => {
+    document.querySelector('#menu').classList.remove('fa-times');
+    document.querySelector('header').classList.remove('toggle');
   });
 });
